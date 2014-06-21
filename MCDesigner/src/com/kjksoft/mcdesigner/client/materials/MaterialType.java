0011e47f219473460f1f3eaeedba2f87c861c28a
Kevin K <kcraft.kevin@gmail.com>
2014-06-21 17:05:08 -0500

    Progress toward full materials list

--- MCDesigner/src/com/kjksoft/mcdesigner/client/materials/MaterialType.java
+++ MCDesigner/src/com/kjksoft/mcdesigner/client/materials/MaterialType.java
@@ -4,7 +4,11 @@ package com.kjksoft.mcdesigner.client.materials;
 public enum MaterialType {
 	NATURAL("Natural"),
 	CRAFTED("Crafted"),
+	PLANT("Plant"),
+	REDSTONE("Redstone"),
+	ORE("Ore"),
 	WOOL("Wool"),
+	APPARATUS("Apparatus"),
 	;
 	
 	private final String title;
