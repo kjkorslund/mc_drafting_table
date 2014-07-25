c9b0139dfc1ee2520571b2d5df421efd3fc18f81
Kevin K <kcraft.kevin@gmail.com>
2014-07-25 05:01:12 -0500

    Converted BannerBuilder entry point into a UIBinder composite

--- BannerBuilder/src/com/mcdraftingtable/bannerbuilder/client/BannerBuilder.java
+++ BannerBuilder/src/com/mcdraftingtable/bannerbuilder/client/BannerBuilder.java
@@ -1,16 +1,31 @@
 package com.mcdraftingtable.bannerbuilder.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class BannerBuilder implements EntryPoint {
+public class BannerBuilder extends Composite implements EntryPoint {
 
+	interface BannerBuilderUiBinder extends UiBinder<Widget, BannerBuilder> {
+	}
+
+	private static BannerBuilderUiBinder uiBinder = GWT
+			.create(BannerBuilderUiBinder.class);
+	
+	public BannerBuilder() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		
+		RootLayoutPanel.get().add(this);
 	}
 }
