45931e4593f66191a339c0e88b555df2f35e995c
Kevin K <kcraft.kevin@gmail.com>
2014-07-24 22:51:39 -0500

    Initial commit for BannerBuilder

---
+++ BannerBuilder/src/com/mcdraftingtable/bannerbuilder/client/BannerBuilder.java
@@ -0,0 +1,16 @@
+package com.mcdraftingtable.bannerbuilder.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class BannerBuilder implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		
+	}
+}
