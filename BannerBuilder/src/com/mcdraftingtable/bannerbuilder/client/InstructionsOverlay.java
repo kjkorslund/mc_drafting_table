6a7dc7200038c4af1fb982bf7d2c9469bb93a665
Kevin K <kcraft.kevin@gmail.com>
2014-08-18 23:13:33 -0500

    Added test RecipePanel to the InstructionsOverlay panel

--- BannerBuilder/src/com/mcdraftingtable/bannerbuilder/client/InstructionsOverlay.java
+++ BannerBuilder/src/com/mcdraftingtable/bannerbuilder/client/InstructionsOverlay.java
@@ -2,8 +2,10 @@ package com.mcdraftingtable.bannerbuilder.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.mcdraftingtable.bannerbuilder.client.ui.RecipePanel;
 
 public class InstructionsOverlay extends Composite {
 
@@ -13,6 +15,8 @@ public class InstructionsOverlay extends Composite {
 	interface InstructionsOverlayUiBinder extends
 			UiBinder<Widget, InstructionsOverlay> {
 	}
+	
+	@UiField RecipePanel recipePanel;
 
 	public InstructionsOverlay() {
 		initWidget(uiBinder.createAndBindUi(this));
