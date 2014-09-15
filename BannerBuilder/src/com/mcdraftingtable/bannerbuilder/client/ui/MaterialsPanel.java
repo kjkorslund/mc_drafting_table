e3483a561b67fad60195924c795d735cf0ccbf9f
Kevin K <kcraft.kevin@gmail.com>
2014-09-15 14:54:21 -0500

    Fixed MaterialsPanel to use a Grid instead of a FlowPanel (better alignment)

--- BannerBuilder/src/com/mcdraftingtable/bannerbuilder/client/ui/MaterialsPanel.java
+++ BannerBuilder/src/com/mcdraftingtable/bannerbuilder/client/ui/MaterialsPanel.java
@@ -1,15 +1,19 @@
 package com.mcdraftingtable.bannerbuilder.client.ui;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Widget;
 import com.mcdraftingtable.bannerbuilder.client.image.ImageBuffer;
 
 public class MaterialsPanel extends Composite {
+	private static final int MATERIALS_LIST_NCOLS = 5;
 
 	private static MaterialsPanelUiBinder uiBinder = GWT
 			.create(MaterialsPanelUiBinder.class);
@@ -17,26 +21,49 @@ public class MaterialsPanel extends Composite {
 	interface MaterialsPanelUiBinder extends UiBinder<Widget, MaterialsPanel> {
 	}
 	
-	@UiField FlowPanel materialsList;
-
+	@UiField Grid materialsList;
+	
 	public MaterialsPanel() {
 		initWidget(uiBinder.createAndBindUi(this));
 
-		// FIXME [kjk] The problem with FlowPanel is that the cells don't line
-		// up vertically due to varying cell widths. It may be more complex to
-		// implement, but I think it has to be a grid.
+		ArrayList<MaterialCount> testMaterialCounts = new ArrayList<>();
 		for(int i=0; i<12; i++) {
-			MaterialCount materialCount = new MaterialCount();
-			
-			ImageBuffer testIcon = new ImageBuffer();
-			String uri = "res/image/items/barrier.png";
-			testIcon.loadFromImgSrc(uri);
-			materialCount.setMaterialCount(testIcon, i+5);
-			materialCount.getElement().getStyle().setMarginRight(8.0, Unit.PX);
-			materialCount.getElement().getStyle().setMarginBottom(2.0, Unit.PX);
-			
-			materialsList.add(materialCount);
+			MaterialCount materialCount = createTestMaterialCount(i+5);
+			testMaterialCounts.add(materialCount);
 		}
+		setMaterialsListContents(testMaterialCounts);
+	}
+	
+	public void setMaterialsListContents(Collection<MaterialCount> materialCounts) {
+		resizeMaterialsList(materialCounts.size());
+		int row = 0;
+		int col = 0;
+		for(MaterialCount materialCount : materialCounts) {
+			materialsList.setWidget(row, col, materialCount);
+			if (++col >= MATERIALS_LIST_NCOLS) {
+				col = 0;
+				row++;
+			}
+		}
+	}
+	
+	private void resizeMaterialsList(int cellCount) {
+		int nRows = cellCount / MATERIALS_LIST_NCOLS;
+		if (cellCount % MATERIALS_LIST_NCOLS > 0) nRows++;
+		materialsList.resize(nRows, MATERIALS_LIST_NCOLS);
+	}
+	
+	private static MaterialCount createTestMaterialCount(int count) {
+		MaterialCount materialCount = new MaterialCount();
+		
+		ImageBuffer testIcon = new ImageBuffer();
+		String uri = "res/image/items/barrier.png";
+		testIcon.loadFromImgSrc(uri);
+		materialCount.setMaterialCount(testIcon, count);
+		materialCount.getElement().getStyle().setMarginRight(8.0, Unit.PX);
+		materialCount.getElement().getStyle().setMarginBottom(2.0, Unit.PX);
+		
+		return materialCount;
 	}
 
 }
